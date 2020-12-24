8b4e04a37ab4cf98086be7f5072faa83c01c510d
James McNabb <jack.mcnabb@gmail.com>
2020-12-24 15:30:43 -0500

    add login ui

---
+++ coinStack/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Blazored.LocalStorage;
+
+namespace coinStack.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorageService;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+
+            if (await _localStorageService.GetItemAsync<bool>("isAuthed"))
+            {
+                var identity = new ClaimsIdentity(
+                    new[]
+                    {
+                    new Claim(ClaimTypes.Name, "James")
+                    }, "test auth type");
+
+                var user = new ClaimsPrincipal(identity);
+                state = new AuthenticationState(user);
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+    }
+}
--- coinStack/Client/Program.cs
+++ coinStack/Client/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +19,11 @@ namespace coinStack.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
             await builder.Build().RunAsync();
         }
