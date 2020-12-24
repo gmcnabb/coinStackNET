29afb0c9b07cdfe48913c644d8c5352162d3e028
James McNabb <jack.mcnabb@gmail.com>
2020-12-24 18:52:28 -0500

    set up db stuff for first migration

---
+++ coinStack/Shared/UserWatchlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinStack.Shared
+{
+    public class UserWatchlist
+    {
+        public int Id { get; set; }
+        public int WatchlistId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ coinStack/Shared/Watchlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinStack.Shared
+{
+    public class Watchlist
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public List<Coin> Coins { get; set; }
+    }
+}
